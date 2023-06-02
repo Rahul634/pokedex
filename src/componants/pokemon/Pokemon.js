e508b5d71e659846fd6a553c92a7bffd9a7a450f
Rahul Bhave <rahulbhave35@gmail.com>
2023-06-02 22:40:06 +0530

    modal not working

--- src/componants/pokemon/Pokemon.js
+++ src/componants/pokemon/Pokemon.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
 import {Text, Image, Grid, GridItem, Flex } from '@chakra-ui/react'
 import axios from 'axios';
-
+import Modal from '../modal/Modal'
 function Pokemon({pokemons}) {
 
     const [details, setDetails] = useState();
     const [pokemon, setPokemon] = useState();
     const [modal, setModal] = useState(false);
-
+   
+    document.body.addEventListener('click',(e)=>{
+        if(e.target.classList.contains('modal')){
+            setModal(false);
+        }
+     });
+     
     const handlePokemonClick = (name, url) => {
         const fetchData = async() => {
             const {data} = await axios.get(url);
@@ -19,7 +25,7 @@ function Pokemon({pokemons}) {
         setPokemon(name);
     }
     const renderPokemons = pokemons?.map((pokemon, index) => {
-       return ( <GridItem  w='100%' h='200' bg='white' onClick={handlePokemonClick(pokemon.name, pokemon.url)}>
+       return ( <GridItem  w='100%' h='200' bg='white' key={index} onClick={() => handlePokemonClick(pokemon.name, pokemon.url)}>
             <Flex h='200' alignItems='center' direction='column' justifyContent={'space-around'}>
                 <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.slice(34,pokemon.url.lastIndexOf())}.png`} alt={pokemon.name}/>
              <Text>{pokemon.name}</Text>
@@ -27,7 +33,7 @@ function Pokemon({pokemons}) {
         </GridItem>)
     })
   return (
-
+    <>
     <Grid 
     bg={'tomato'}
     pt={20}
@@ -36,6 +42,10 @@ function Pokemon({pokemons}) {
     >
         {renderPokemons}
    </Grid>
+   <div className={modal?'modal':''}>
+             {modal?<Modal pokemon={pokemon}  details={details}/>:null}
+         </div>
+   </>
   )
 }
 
